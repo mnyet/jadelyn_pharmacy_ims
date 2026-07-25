8354740938646c490fe3e9b3ad61d0b1db25b42f
mnyet <janbercalamba10@gmail.com>
2026-07-26 00:13:00 +0800

    Minor Configurations

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -37,7 +37,9 @@ class Database extends Config
         'charset'      => 'utf8mb4',
         'DBCollat'     => 'utf8mb4_general_ci',
         'swapPre'      => '',
-        'encrypt'      => false,
+        'encrypt'  => [
+            'ssl_verify' => true,
+        ],
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
