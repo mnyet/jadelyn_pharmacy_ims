1b914ef0392bd8b16ad393ac48b0005caf83e2cf
mnyet <janbercalamba10@gmail.com>
2026-09-19 23:38:04 +0800

    Updated the low stock reports and added more functionality to the homepage buttons

---
+++ public/assets/js/jadelyn-pharmacy/Homepage.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    console.log('Low Stock Modal View Loaded.');
+});
+
+function loadLowStockModalView() {
+    $('#lowStockModal').modal('show'); 
+
+    // Populate the transaction items table
+    $('#lowStockTable').DataTable({
+        destroy: true, // Destroy existing instance before reinitializing
+        processing: true,
+        serverSide: true,
+        searching: false,        // ← hides search box
+        lengthChange: false,     // ← hides "entries per page" dropdown
+        paging: false,           // ← hides pagination controls
+        info: false,             // ← hides "Showing X to Y of Z entries"
+        ajax: {
+            url: $('#baseUrl').val() + 'products/get-low-stock-datatable',
+            type: "POST",
+        },
+        layout: {
+            topEnd: null
+        },
+        columns: [ // These matches the columns from the low stock report view.
+            { data: 'Generic_Name', name: 'Generic_Name' },
+            { data: 'Product_Type', name: 'Product_Type' },
+            { data: 'Total_Quantity', name: 'Total_Quantity' },
+        ],
+    });
+}
